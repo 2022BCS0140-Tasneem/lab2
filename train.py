53cc012be65f4af56192f6c22939ce9abdea50f9
Syed Tasneem Kousar <syedtasneemkousar629@gmail.com.com>
2026-01-09 22:29:43 +0530

    Experiment 5: RandomForest baseline

--- train.py
+++ train.py
@@ -8,7 +8,6 @@ os.makedirs("output/model", exist_ok=True)
 os.makedirs("output/results", exist_ok=True)
 
 data = pd.read_csv("dataset/winequality-red.csv", sep=";")
-
 X = data.drop("quality", axis=1)
 y = data["quality"]
 
@@ -16,22 +15,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestRegressor(
-    n_estimators=100,
-    random_state=42
-)
-
+model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+metrics = {
+    "MSE": mean_squared_error(y_test, y_pred),
+    "R2": r2_score(y_test, y_pred)
+}
 
 joblib.dump(model, "output/model/model.pkl")
+json.dump(metrics, open("output/results/metrics.json", "w"), indent=4)
 
-with open("output/results/metrics.json", "w") as f:
-    json.dump({"MSE": mse, "R2": r2}, f, indent=4)
-
-print("MSE:", mse)
-print("R2:", r2)
+print(metrics)
