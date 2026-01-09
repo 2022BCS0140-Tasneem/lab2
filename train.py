484d644016458d7a089b85b4031582b6be925f18
Syed Tasneem Kousar <syedtasneemkousar629@gmail.com.com>
2026-01-09 22:31:55 +0530

    Experiment 6: RandomForest with data split strategy (test_size=0.3)

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ X = data.drop("quality", axis=1)
 y = data["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.3, random_state=42
 )
 
 model = RandomForestRegressor(random_state=42)
